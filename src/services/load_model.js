d3bbfe98bd03e67026166e8225fd3de895517f7a
Al Hilaluddin <shiigetsu1@gmail.com>
2024-05-10 19:20:43 +0800

    finally

--- src/services/load_model.js
+++ src/services/load_model.js
@@ -1,5 +1,6 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    const modelUrl = process.env.MODEL_URL || "https://storage.googleapis.com/submissionmlgc-alhilaluddin/submissions-model/model.json";
+    return tf.loadGraphModel(modelUrl);
 }
 module.exports = loadModel;
